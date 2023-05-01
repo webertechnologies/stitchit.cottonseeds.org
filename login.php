06590c49913c86a1af9b8c7fa826b26328994fe9
kiranpranay <pranaybannu12@gmail.com>
2023-05-02 00:18:50 +0530

    fix login screen in mobile view
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- login.php
+++ login.php
@@ -28,6 +28,9 @@ if(isset($_POST['submit'])){
 
 <head>
     <!-- Design by foolishdeveloper.com -->
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Login</title>
 
     <link rel="preconnect" href="https://fonts.gstatic.com" />
