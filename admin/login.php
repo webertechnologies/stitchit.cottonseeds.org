34fd5a3cf6ca37f4b374b750a3f5867ef511eadb
kiranpranay <pranaybannu12@gmail.com>
2023-05-02 11:25:57 +0530

    Fix login screen in admin
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- admin/login.php
+++ admin/login.php
@@ -49,7 +49,9 @@ if(isset($_POST["submit"])){
 <html lang="en" dir="ltr">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Admin Login</title>
     <link rel="stylesheet" href="assets/css/log-style.css">
 </head>
